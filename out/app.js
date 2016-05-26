78084616e5ff627331c0dc65793c937c9377094f
Wang Zishi <ynh.2@outlook.com>
2016-05-26 15:14:57 +0800

    Update

---
+++ out/app.js
@@ -0,0 +1,69 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator.throw(value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments)).next());
+    });
+};
+const http = require('http');
+const pdf = require('html-pdf');
+const formBody = require('body/form');
+const jsonBody = require('body/json');
+const server = http.createServer((req, res) => __awaiter(this, void 0, void 0, function* () {
+    if (req.method === 'GET' && req.url === '/health') {
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify({ status: 'ok' }));
+    }
+    else if (req.method === 'POST' && req.url === '/pdf') {
+        if (req.headers['content-type'] !== 'application/json' &&
+            req.headers['content-type'] !== 'application/x-www-form-urlencoded') {
+            res.statusCode = 415;
+            res.end();
+        }
+        else {
+            let body, html, options, generatePdf = (html, options, res) => {
+                let filename = options.filename || 'render';
+                try {
+                    pdf.create(html, {
+                        format: 'A4'
+                    }).toStream((err, stream) => {
+                        if (!!err)
+                            res.end(err);
+                        else {
+                            res.setHeader('Content-Type', 'application/pdf');
+                            res.setHeader('Content-Disposition', `attachment;filename="${filename}.pdf"`);
+                            stream.pipe(res);
+                        }
+                    });
+                }
+                catch (error) {
+                    res.statusCode = 500;
+                    res.setHeader('Content-Type', 'application/json');
+                    res.end(JSON.stringify({ error: error.message }));
+                }
+            };
+            if (req.headers['content-type'] === 'application/json')
+                jsonBody(req, res, (err, body) => {
+                    console.log({ jsonBody: body });
+                    generatePdf(body.html, body.options, res);
+                });
+            else {
+                formBody(req, res, (err, body) => {
+                    body.html = decodeURIComponent(body.html);
+                    body.options = JSON.parse(body.options);
+                    console.log({ formOptions: body.options });
+                    generatePdf(body.html, body.options, res);
+                });
+            }
+        }
+    }
+    else {
+        res.statusCode = 404;
+        res.end();
+    }
+}));
+server.listen(3000);
+console.log('Listening on port 3000...');
+//# sourceMappingURL=app.js.map
